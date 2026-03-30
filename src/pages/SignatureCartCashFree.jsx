7d49c7b2c3b6d2e2c081980a915612599e9323d9
Skysell01 <Aarohi@skysellmedia.com>
2026-03-30 16:50:07 +0530

    489

--- src/pages/SignatureCartCashFree.jsx
+++ src/pages/SignatureCartCashFree.jsx
@@ -34,7 +34,7 @@ function SignatureCartCashFree() {
       name: "✍ Professional Signature Design",
       description:
         "Personalized professional signature design based on your name and personality",
-      price: 3,
+      price: 489,
       originalPrice: 4999,
       duration: "24-48 hours",
       features: [
